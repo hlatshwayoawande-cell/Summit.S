1d928380d9de3d627bd71ae446052877f3790d05
hlatshwayoawande-cell <hlatshwayoawande@gmail.com>
2026-08-01 18:55:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,9 +39,9 @@ TELEGRAM_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '')
 MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
 MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
 MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True') == 'True'
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME', '')
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', '')
-MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', 'noreply@summit.app')
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME' '')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD' '')
+MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', 'hlatshwayoawande@gmail.com')
 FEEDBACK_RECIPIENT = os.environ.get('FEEDBACK_RECIPIENT', 'support@summit.app')
 
 MARKET_STOCKS = [
